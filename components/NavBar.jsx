9b827db5660b59d2e7d6cfacdeb90c3470420da0
Axel Castro <47039794+AyyJayyC@users.noreply.github.com>
2026-05-15 15:26:12 -0700

    feat: increase logo size for better navbar visibility

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -70,8 +70,8 @@ export default function NavBar() {
       <div className="container max-w-7xl">
         <div className="flex items-center justify-between h-16">
           <Link href="/" className="inline-flex items-center" aria-label="Inicio Casa-MX.com">
-            <img src="/brand/logo-light.png" alt="CASA MX" className="block dark:hidden h-9 w-auto md:h-10" />
-            <img src="/brand/logo-dark.png" alt="CASA MX" className="hidden dark:block h-9 w-auto md:h-10" />
+            <img src="/brand/logo-light.png" alt="CASA MX" className="block dark:hidden h-11 w-auto md:h-12" />
+            <img src="/brand/logo-dark.png" alt="CASA MX" className="hidden dark:block h-11 w-auto md:h-12" />
           </Link>
 
           <DesktopNavLinks
