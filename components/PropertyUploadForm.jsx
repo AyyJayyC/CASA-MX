ba98142072b1abc2a2b61035c8b7716ce47191eb
Axel Castro <47039794+AyyJayyC@users.noreply.github.com>
2026-05-18 09:33:51 -0700

    fix: restore to last known working version (badges commit)

--- components/PropertyUploadForm.jsx
+++ components/PropertyUploadForm.jsx
@@ -1109,7 +1109,7 @@ export default function PropertyUploadForm({ listingType = 'for_sale' }) {
             )}
           </div>
 
-          {/* {/* Location fields */}
+          {/* Location fields */}
           <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
             <div>
               <label htmlFor="estado" className={labelClass}>
@@ -1582,6 +1582,7 @@ export default function PropertyUploadForm({ listingType = 'for_sale' }) {
           >
             Limpiar formulario
           </button>
+        </div>
         </div>
       </form>
       )}
